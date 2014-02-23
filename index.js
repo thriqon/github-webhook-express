1b460c4db7295b043dc43328fd06140333f885b9
thriqon <github@jonasw.de>
2014-02-23 21:47:43 +0100

    staging

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 
+/*jslint indent:2 */
+
 "use strict";
 
-var Netmask = require('netmask').Netmask;
-
-var request = require('request');
+var Netmask = require('netmask').Netmask,
+  request = require('request');
 
 module.exports = function () {
-	var _validIpRanges = [new Netmask('192.30.252.0/22')];
+  var validIpRanges = [new Netmask('192.30.252.0/22')],
+    res = function (req, eres, next) {
+      req.githubevent = req.headers['x-github-event'] || null;
+      req.githubdeliveryid = req.headers['x-github-delivery'] || null;
+      req.isByGithub = validIpRanges.some(function (range) { return range.contains(req.socket.remoteAddress); });
+      next();
+    };
 
-	var res = function (req, res, next) {
-		req.githubevent = req.headers['x-github-event'] || null;
-		req.githubdeliveryid = req.headers['x-github-delivery'] || null;
-		req.isByGithub = _validIpRanges.some(function (range) { return range.contains(req.socket.remoteAddress); });
-		next();
-	};
+  res.refreshAllowedIps = function (callback) {
+    request(process.env.npm_package_config_githubmetaapi, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        validIpRanges = JSON.parse(body).hooks.map(function (str) { return new Netmask(str); });
+        callback(null, res);
+      } else {
+        callback(error);
+      }
+    });
+  };
 
-	res.refreshAllowedIps = function (callback) {
-		request(process.env.npm_package_config_githubmetaapi, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				_validIpRanges = JSON.parse(body).hooks.map(function (str) { return new Netmask(str); });
-				callback(null, res);
-			} else {
-				callback(error);
-			}
-		});
-	};
-
-	res.validIpRanges = function (newrange) {
-		if (newrange instanceof Array) {
-			_validIpRanges = newrange;
-		} else {
-			return _validIpRanges;
-		}
-	};
-	return res;		
+  res.validIpRanges = function (newrange) {
+    if (newrange instanceof Array) {
+      validIpRanges = newrange;
+    } else {
+      return validIpRanges;
+    }
+  };
+  return res;
 };
